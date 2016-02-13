d6c18746a83ca6bf38cd85a47d9293612fad8e8c
hanakamer <hanakamer@gmail.com>
2016-02-13 12:54:56 +0200

    admin panel modified

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Post
+from django.contrib.auth.models import User
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date')
+    search_fields = ['title']
+    fields = ('title', 'text', 'author', 'created_date')
+    readonly_fields = ('created_date',)
+    # exclude = ('created_date',)
+
+admin.site.register(Post, PostAdmin)
+# admin.site.register(User)
